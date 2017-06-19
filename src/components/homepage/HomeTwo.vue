66a33d3bded24ea49c3897bd1feb8e500bde5be3
patwill92 <patrick.rw92@gmail.com>
2017-06-18 22:47:10 -0400

    Add Home 2

---
+++ src/components/homepage/HomeTwo.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div class = "hp2-content">
+            <p :class = 'icon'></p>
+            <div class = "heading">
+                <p>{{title}}</p>
+            </div>
+            <div class = "info">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla.</p>
+            </div>
+        </div>
+
+        <div class = "divider"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['icon', 'title', 'line']
+    }
+</script>
+
+<style>
+    .hp2-content{
+        width: 80%;
+        margin: auto;
+    }
+
+    .i{
+        font-size: 3.0rem;
+        margin-top: 0;
+        margin-bottom: 20px;
+        color: #003691;
+    }
+
+    .heading{
+        text-transform: uppercase;
+        font-weight: 300;
+        font-size: 1.6rem;
+    }
+
+    .info{
+        font-weight: 300;
+        letter-spacing: 1.5px;
+    }
+
+
+    .divider{
+        width: 1px;
+        border-right: 1px solid #B9B9B9;
+    }
+</style>
