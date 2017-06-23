3cebe356452f7f749ed4d1b875146c9babdaf403
patwill92 <patrick.rw92@gmail.com>
2017-06-23 00:53:44 -0400

    Add Home 2

--- src/components/contact/Navbar.vue
+++ src/components/contact/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="navbar">
         <div class="nav-left">
-            <router-link to="/" activeClass="active"><a href="#">tua construction</a></router-link>
+            <router-link to="/" activeClass="active"><a>tua construction</a></router-link>
         </div>
         <div class="right">
-            <a href="#">Services</a>
-            <a href="#">About Us</a>
-            <router-link to="/contact" activeClass="active"><a href="#">Contact</a></router-link>
-            <a href="#"><i class = "fa fa-facebook"></i></a>
+            <a>Services</a>
+            <a>About Us</a>
+            <router-link to="/contact" activeClass="active"><a>Contact</a></router-link>
+            <a><i class = "fa fa-facebook"></i></a>
         </div>
     </div>
 </template>
